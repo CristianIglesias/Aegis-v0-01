d169a03d1208af8312394452cce3369ae30d876a
CristianIglesias <Iglesias.cristian99@gmail.com>
2020-06-20 22:38:05 -0300

    Avanzando Carga Cliente
    Avanzando con la carga de clientes.
    no llegué tan lejos igual :(

--- AEGIS V0.01/Clientes.cpp
+++ AEGIS V0.01/Clientes.cpp
@@ -4,11 +4,14 @@ using namespace std;
 #include "Clientes.h"
 #include "PrototiposGlobales.h"
 const char *ArchivoClientes ="Clientes.dat";
-
+/// A SABER
+/// ERROR=0-TA TODO PIOLA, SIGUE ADELANTE
+/// ERROR=1-SALIDA VOLUNTARIA, CLAVA RETURN PERRI
+/// ERROR<0-SIGUE INTENTANDO, TRANK PALANK
 void  Cliente:: cargar()///Carga Cliente.
 {
     Persona::Cargar();
-    idCliente=Generarid();
+    Generarid();
     if(idCliente<0)
     {
         cout<<"Hubo un error en la generación de ID Cliente, Intente Nuevamente"<<endl;
@@ -17,17 +20,94 @@ void  Cliente:: cargar()///Carga Cliente.
     cout<<"ID Cliente: "<<idCliente;
     cout<<"Tipo De Pago Preferido: ";
     SetTipoPago();
-
-
-
-    ///sacando que faltan cosas
-
+    ///TODO COMPLETAR CARGA CLIENTA
 };
 
 void  Cliente:: mostrar()
 {
     Persona::Mostrar();
+
+
 };
+
+int  Cliente:: SetTipoPago()
+{
+    int error=0,i=0;
+    while(error<0)
+        cin>>TipodePago;
+    error=ValidarTipoPago();
+    if(error==1)
+    {
+        return 1;
+    }
+
+    if(error<0)
+    {
+        i++;
+        cout<<"Error Validando Tipo Pago -Nro "<<error<<" Intente Nuevamente."<<endl;
+        cout<<"Intento Numero "<<i<<"/3"<<endl;
+        if(i>=3)
+        {
+            cout<<"Si querés salir, ingresá 99"<<endl;
+        }
+    }
+
+};
+
+int Cliente :: ValidarTipoPago()
+{
+    int error;
+    if(TipodePago>1&&TipodePago%1!=0)
+    {
+        error=0;
+        return error;
+    }
+    if(TipodePago==99)
+    {
+        error=1;
+        return error;
+    }
+
+    else
+    {
+        error=-1;
+        return error;
+    }
+}
+
+int Cliente :: SetPrefFact()
+{
+///TODO INGRESO Y VALIDACIÓN, CON SALIDA A VOLUNTAD.
+};
+
+int Cliente :: ValidarPrefFact()
+{
+    int error;
+    return error;
+};
+
+
+void Cliente :: SetDeuda()
+{
+///TODO INGRESO Y VALIDACIÓN, CON SALIDA A VOLUNTAD.
+};
+void Cliente::Generarid()
+{
+    int id=0,cantRegistros=0;
+    FILE *p;
+    Cliente aux;
+    p=fopen(ArchivoClientes,"wb");
+    if(p==NULL)
+        idCliente=-1;
+    return;
+    fseek(p,0,2);
+    cantRegistros=ftell(p)/sizeof(Cliente);
+
+    id=cantRegistros+1;
+    fclose(p);
+    idCliente=id;
+    return;
+}
 int   Cliente:: GetidCliente()
 {
     return idCliente;
@@ -44,33 +124,3 @@ float Cliente:: GetDeuda()
 {
     return deuda;
 };
-void  Cliente:: SetTipoPago(int NuevoTipoP)
-{
-
-};
-void Cliente :: SetPrefFact(int NuevoPrefFact)
-{
-
-};
-void Cliente :: SetDeuda(float NuevaDeuda)
-{
-
-};
-void Cliente::Generarid()
-{
-    int id,cantRegistros=0;
-    FILE *p;
-    Cliente aux;
-    p=fopen(ArchivoClientes,"ab");
-    if(p==NULL)
-        idCliente=-1;
-    return;
-    while(fread(&aux,sizeof(Cliente),1,p)==1)
-    {
-        cantRegistros++;
-    }
-    id=cantRegistros+1;
-    fclose(p);
-    idCliente=id;
-    return;
-}
